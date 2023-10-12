7286d6c5233d6708e541081481d11cd092aafc6a
experiment1234 <experiment123@naver.com>
2023-10-12 17:36:13 +0900

    20231012

--- 11_mybatis_mockmvc/src/main/java/com/gdu/app11/dto/ContactDto.java
+++ 11_mybatis_mockmvc/src/main/java/com/gdu/app11/dto/ContactDto.java
@@ -4,15 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class ContactDto {
-  private int contact_no;
+  private int contactNo;
   private String name;
   private String tel;
   private String email;
   private String address;
-  private String created_at;
+  private String createdAt;
 }
