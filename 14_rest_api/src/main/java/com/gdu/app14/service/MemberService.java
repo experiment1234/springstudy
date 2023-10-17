6105874a929f5da25cb7313889b34c4039f898ad
experiment1234 <experiment123@naver.com>
2023-10-17 17:48:50 +0900

    20231017

--- 14_rest_api/src/main/java/com/gdu/app14/service/MemberService.java
+++ 14_rest_api/src/main/java/com/gdu/app14/service/MemberService.java
@@ -1,29 +1,29 @@
 package com.gdu.app14.service;
 
-import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.gdu.app14.dto.MemberDto;
 
 public interface MemberService {
 
-
   // 회원 등록
   public Map<String, Object> register(MemberDto memberDto, HttpServletResponse response);
   
   // 회원 목록
-  public List<MemberDto> getMembers(int page);
+  public Map<String, Object> getMembers(int page);
   
+  // 회원 조회
+  public Map<String, Object> getMember(int memberNo);
   
+  // 회원 정보 수정
+  public Map<String, Object> modifyMember(MemberDto memberDto);
   
+  // 회원 정보 삭제
+  public Map<String, Object> removeMember(int memberNo);
   
-  
-  
-  
-  
-  
+  // 회원들 정보 삭제
+  public Map<String, Object> removeMembers(String memberNoList);
   
 }
